981905d0da067d3ecebd29ea7e99678ef635d77b
Samir Gezalov <samir.gezalov@gmail.com>
2026-05-15 14:08:27 +0400

    feat: pages build with vals

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   await Supabase.initialize(
-    url: 'https://ckoynmulxrrtkhmyvmhd.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrb3lubXVseHJydGtobXl2bWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NDEyMTUsImV4cCI6MjA3ODExNzIxNX0.viqmVz-FzgT3ECs8sI0d_ijmXea8BtwVlf5uZfnxBPc',
+    url: const String.fromEnvironment('SUPABASE_URL',),
+    anonKey: const String.fromEnvironment('SUPABASE_ANON_KEY', ),
   );
 
   // Ensure we have a session for Realtime/RLS
@@ -35,7 +35,7 @@ class SOSEarthApp extends ConsumerWidget {
     final inRoom = ref.watch(roomProvider.select((state) => state.inRoom));
 
     return MaterialApp(
-      title: 'S.O.S. Web Conference',
+      title: 'Web Conference',
       theme: AppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       home: inRoom ? const RoomScreen() : const LoginScreen(),
